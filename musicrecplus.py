2a931652144c5de8f7687b9ddcc0563af81c83b2
Mikayla M <54966992+verdetea22@users.noreply.github.com>
2022-04-12 18:00:51 -0400

    Update musicrecplus.py

--- musicrecplus.py
+++ musicrecplus.py
@@ -1,1 +1,80 @@
+#  Mikayla Mount
+# CS115 
+# I pledge my Honor that I have abided by the Stevens Honor System.
+
+
+ 
+def preference_input(db, username):
+    """prompts user to enter their liked artists, replacing old preferences if they exist
+    input db= database storing user data  (string)
+    input username = specific user id (string)
+    author: Mikayla M
+    """
+    pass
+
+
+
+def print_preference(db, username):
+""" returns users most current artist preferences
+    input db= database storing user data  (string)
+    input username = specific user id (string)
+    author: Mikayla M
+    """
+    pass
+
+
+
+def delete_preference(db, username):
+ """ returns users preferences and deletes specific artists based on input
+    input db= database storing user data  (string)
+    input username = specific user id (string)
+    author: Mikayla M
+    """
+    pass
+
+
+def create_reccomendations(db, username):
+    """ returns reccommended artists, if no preferences then return no rec
+    input db= database storing user data  (string)
+    input username = specific user id (string)
+    author: 
+    """
+    pass
+
+
+
+
+def most_popular(db):
+    """returns most popular artist 
+     input db= database storing user data  (string)
+     author: 
+    """
+    pass
+
+
+
+def most_popular_stat(db):
+    """returns how many people like the most popular artist
+    input db= database storing user data  (string)
+     author: 
+    """
+    pass
+
+
+
+def user_most_artists(db):
+    """ returns user with the most preferences, one per line and sorted if more than one
+    input db= database storing user data  (string)
+    author: 
+    """
+    pass
+
+
+
+
+def main():
+    """ main method for menu, files, database and username
+    author: Mikayla M
+    """
+    pass
 
