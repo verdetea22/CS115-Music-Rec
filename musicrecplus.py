fd9968ddb8e3cbb5a780054ce203db0806b551dd
Mikayla M <54966992+verdetea22@users.noreply.github.com>
2022-04-14 19:51:32 -0400

    Update musicrecplus.py

--- musicrecplus.py
+++ musicrecplus.py
@@ -3,6 +3,7 @@
 # I pledge my Honor that I have abided by the Stevens Honor System.
 
 import os.path
+###################################################
 
 def loadUsers(fileName):
     """Reads in a file of stores users' preferences stored
@@ -21,6 +22,8 @@ def loadUsers(fileName):
     file.close()
     return userDict
 
+###################################################
+
 def saveUserPreferences(userName, prefs, userMap, fileName):
     """Writes all of the user preferences to the file
        Returns nothing.
@@ -33,81 +36,109 @@ def saveUserPreferences(userName, prefs, userMap, fileName):
                  "\n"
         file.write(toSave)
     file.close()
- 
-def preference_input(db, username):
+    
+ ###################################################
+    
+def preference_input(userName, userMap):
     """prompts user to enter their liked artists, replacing old preferences if they exist
-        input db= database storing user data  (string)
-        input username = specific user id (string)
+        input userMap= list storing user data  []
+        input userName = specific user id (string)
         author: Mikayla M
     """
-    pass
+    bandList = []
+    band = input("Enter an artist that you like, or press Enter to quit  : " )
+    
+    while band!="":
+        bandList.append(band.strip().title())
+        band = input("Enter another artist that you like, or press Enter to quit : " )
+        
+    bandList.sort()
+    userMap[userName] = bandList
 
+    return userMap
+    
+###################################################
 
-
-def print_preference(db, username):
+def print_preference(userName, userMap) :
     """ returns users most current artist preferences
-        input db= database storing user data  (string)
-        input username = specific user id (string)
+        input userMap= list storing user data  []
+        input userName = specific user id (string)
         author: Mikayla M
-    """
+  """
     pass
 
+###################################################
 
-
-def delete_preference(db, username):
+def delete_preference(userName, userMap):
     """ returns users preferences and deletes specific artists based on input
-        input db= database storing user data  (string)
-        input username = specific user id (string)
+        input userMap= list storing user data  []
+        input userName = specific user id (string)
         author: Mikayla M
     """
     pass
 
+###################################################
 
-def create_reccomendations(db, username):
+def create_reccomendations(userName, userMap):
     """ returns reccommended artists, if no preferences then return no rec
-        input db= database storing user data  (string)
-        input username = specific user id (string)
+        input userMap= list storing user data  []
+        input userName = specific user id (string)
         author: 
     """
     pass
 
+###################################################
 
-
-
-def most_popular(db):
+def most_popular(userMap):
     """returns most popular artist 
-       input db= database storing user data  (string)
+       input userMap= list storing user data  []
        author: 
     """
     pass
 
+###################################################
 
-
-def most_popular_stat(db):
+def most_popular_stat(userMap):
     """returns how many people like the most popular artist
-       input db= database storing user data  (string)
+       input userMap= list storing user data  []
        author: 
     """
     pass
 
+###################################################
 
-
-def user_most_artists(db):
+def user_most_artists(userMap):
     """ returns user with the most preferences, one per line and sorted if more than one
-        input db= database storing user data  (string)
+        input userMap= list storing user data  []
         author: 
     """
     pass
 
+###################################################
 
-
-
+def menu():
+    """returns menu options
+    author: Mikayla M """""
+    
+    print("""Enter a letter to choose an option:
+    e - Enter preferences
+    r - Get recommendations
+    p - Show most popular artists
+    h - How popular is the most popular
+    m - Which user has the most likes
+    q - Save and quit""")
+    
 def main():
     """ main method for menu, files, database and username
         authors: Mikayla M & Joseph C
     """
-    userData = loadUsers('musicrecplus.txt')
-        
-    
+    #userData = loadUsers('musicrecplus.txt') // was giving error
+    username = input("Hi  user! \nEnter your username, and use $ as a suffix if you would like you preferences to be private :  "    )   
+    menu()
+                   
+                     
+                        
+
+###################################################            
 
 main()
