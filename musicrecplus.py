c55e9b1d574e027b4be2a37d81820f042dcc7c19
Mikayla M <54966992+verdetea22@users.noreply.github.com>
2022-04-15 18:18:47 -0400

    fixed menu func, changed parameters

--- musicrecplus.py
+++ musicrecplus.py
@@ -54,7 +54,7 @@ def preference_input():
     
     while True:
 
-        band = input("Enter an artist that you like, or press Enter to quit  : " )
+        band = input("Enter an artist that you like (Enter to finish)  : " )
 
         if  band!="":
             prefs.append(band.strip().title())
@@ -103,7 +103,7 @@ def delete_preference(userName, userMap):
 
 ###################################################
 
-def create_reccomendations(userName, userMap):
+def create_reccomendations():
     """ returns reccommended artists, if no preferences then return no rec
         input userMap= list storing user data  []
         input userName = specific user id (string)
@@ -113,7 +113,7 @@ def create_reccomendations(userName, userMap):
 
 ###################################################
 
-def most_popular(userMap):
+def most_popular():
     """returns most popular artist 
        input userMap= list storing user data  []
        author: 
@@ -122,7 +122,7 @@ def most_popular(userMap):
 
 ###################################################
 
-def most_popular_stat(userMap):
+def most_popular_stat():
     """returns how many people like the most popular artist
        input userMap= list storing user data  []
        author: 
@@ -131,7 +131,7 @@ def most_popular_stat(userMap):
 
 ###################################################
 
-def user_most_artists(userMap):
+def user_most_artists():
     """ returns user with the most preferences, one per line and sorted if more than one
         input userMap= list storing user data  []
         author: 
@@ -156,7 +156,9 @@ def main():
     """ main method for menu, files, database and username
         authors: Mikayla M & Joseph C
     """
-    #userData = loadUsers('musicrecplus.txt')
+    if userDict  !={}:
+        userData = loadUsers('musicrecplus.txt')
+
     username = input("Hi  user! \nEnter your username, and use $ as a suffix if you would like your preferences to be private :  "    )   
     menu()
 
@@ -168,16 +170,16 @@ def main():
             preference_input()
 
         elif menu_choice == "r":
-            create_reccomendations(userName, userMap)
+            create_reccomendations()
             
         elif menu_choice == "p":
-            most_popular(userMap)
+            most_popular()
 
         elif menu_choice == "h":
-            most_popular_stat(userMap)
+            most_popular_stat()
 
         elif menu_choice == "m":
-            user_most_artists(userMap)
+            user_most_artists()
 
         else :
             print("Invalid input, please choose an option, or press q to quit")
